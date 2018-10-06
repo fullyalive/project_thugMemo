585b1448e2395ed633fa42941414084aab0473e4
fullyalive <wogud1202@gmail.com>
2018-10-06 10:30:42 +0900

    expo init

--- thugmemo/App.js
+++ thugmemo/App.js
@@ -1,47 +1,11 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-class Greeting extends Component {
-  static defaultProps = {
-    name: "World"
-  };
-  render() {
-    return <Text>Hello {this.props.name} !</Text>;
-  }
-}
-
-const UserList = ["문대성", "이재형", "김민호", "나플라"];
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
-  handleCountAdd = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
-  handleCountMinus = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
-  };
-  handleCountReset = () => {
-    this.setState({ count: 0 });
-  };
-
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Greeting name="이재형" />
-        {UserList.map((user, index) => {
-          return <Greeting name={user} key={index} />;
-        })}
-        <Text>Count : {this.state.count}</Text>
-        <Button onPress={this.handleCountAdd} title="더하기" style={styles.button}/>
-        <Button onPress={this.handleCountMinus} title="빼기" color="#3fa9fd" />
-        <Button onPress={this.handleCountReset} title="리셋" color="#3fa9fd" />
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
@@ -50,12 +14,8 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  button: {
-    backgroundColor: "red",
-    color: "white"
-  }
 });
